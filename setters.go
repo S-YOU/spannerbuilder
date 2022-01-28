0c0906c05a7b3d0d57e40f1c7015ed99f3e8a824
S-YOU <S-YOU@users.noreply.github.com>
2022-01-28 20:06:35 +0900

    fix x.* bug

--- setters.go
+++ setters.go
@@ -27,10 +27,8 @@ func (b *Builder) Select(s string, cols ...string) *Builder {
 	// check for backward compatibility
 	if len(cols) == 0 {
 		b.sel = s
-		if strings.IndexByte(s, ',') < 0 {
+		if strings.IndexByte(s, ',') < 0 && strings.IndexByte(s, '.') < 0 {
 			b.cols = []string{s} // simple query with one field selected `.Select("field_name")`
-		} else {
-			b.cols = []string{} // complex query, no column names will be stored in `cols` variable
 		}
 	} else {
 		b.sel = s
