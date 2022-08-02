9482c868b6ffd1a10b004e5daed7abc944893d0a
-tmp <tmp@git>
2022-08-02 15:16:12 +0900

    update 20220802-01

--- spannerbuilder.go
+++ spannerbuilder.go
@@ -3,26 +3,40 @@ package spannerbuilder
 type Builder struct {
 	sql    string
 	table  string
-	sel    string
 	group  string
 	sample string
 	index  string
+	sels   []string
 	cols   []string
+	froms  []string
 	keys   []string
 	wheres []string
 	orders []string
 	joins  []string
 	having []string
+	unions []string
 	limit  int
 	offset int
+	uLim   int
+	uOfs   int
+	uOdrs  []string
 	args   map[string]interface{}
+	tblMap map[string][]string
+	fldMap map[string][]string
 }
 
-func NewSpannerBuilder(table string, cols, keys []string) *Builder {
+func NewSpannerBuilder(table string, cols, keys []string, args ...map[string]interface{}) *Builder {
+	bArgs := make(map[string]interface{})
+	for _, arg := range args {
+		for k, v := range arg {
+			bArgs[k] = v
+		}
+	}
 	return &Builder{
-		table: table,
-		cols:  cols,
-		keys:  keys,
-		args:  make(map[string]interface{}),
+		table:  table,
+		cols:   cols,
+		keys:   keys,
+		args:   bArgs,
+		fldMap: make(map[string][]string),
 	}
 }
