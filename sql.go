f72a6ee0b7d432d2d31dfdb9d844778eb9c68e79
-tmp <tmp@git>
2022-10-06 16:40:17 +0900

    support delete

--- sql.go
+++ sql.go
@@ -16,7 +16,7 @@ func GetInsertSQL(table string, cols, keys []string, values []interface{}) spann
 		setColumns = append(setColumns, c+" = @"+c)
 	}
 
-	sql := `INSERT ` + table + ` (` + strings.Join(cols, ", ") + `) VALUES (@` + strings.Join(cols, ", @") + ")"
+	sql := `INSERT ` + kwQuoted(table) + ` (` + strings.Join(cols, ", ") + `) VALUES (@` + strings.Join(cols, ", @") + ")"
 	stmt := spanner.NewStatement(sql)
 	for i, col := range cols {
 		stmt.Params[col] = values[i]
@@ -41,9 +41,27 @@ func GetUpdateSQL(table string, cols, keys []string, values []interface{}) spann
 		setColumns = append(setColumns, c+" = @"+c)
 	}
 
-	sql := `UPDATE ` + table + ` SET ` + strings.Join(setColumns, ", ") + ` WHERE ` + strings.Join(where, " AND ")
+	sql := `UPDATE ` + kwQuoted(table) + ` SET ` + strings.Join(setColumns, ", ") + ` WHERE ` + strings.Join(where, " AND ")
 	stmt := spanner.NewStatement(sql)
 	for i, col := range cols {
+		stmt.Params[col] = values[i]
+	}
+
+	if debug {
+		log.Printf("SQL: `%s`, Params: %+v\n", sql, stmt.Params)
+	}
+	return stmt
+}
+
+func GetDeleteSQL(table string, keys []string, values []interface{}) spanner.Statement {
+	where := make([]string, len(keys))
+	for i, k := range keys {
+		where[i] = k + " = @" + k
+	}
+
+	sql := `DELETE FROM ` + kwQuoted(table) + ` WHERE ` + strings.Join(where, " AND ")
+	stmt := spanner.NewStatement(sql)
+	for i, col := range keys {
 		stmt.Params[col] = values[i]
 	}
 
